30a2f07cdbb207e566441000b1d1f8cbc088f9b3
Alexander Kahn <alexanderkahn@gmail.com>
2012-01-16 23:09:50 -0500

    Play next video when a video finishes!

--- source/javascripts/players.js
+++ source/javascripts/players.js
@@ -3,26 +3,23 @@ Confluence.Player = function(playlist) {
 }
 
 _.extend(Confluence.Player.prototype, {
+  // Embed video player, begin playing first video
   play: function() {
-    var player = this;
+    var player = this,
+        options = {allowScriptAccess: 'always'};
 
-    swfobject.embedSWF("http://www.youtube.com/v/" + this.playlist.nextId() + "?enablejsapi=1&playerapiid=video&autoplay=1&version=3", "video", "825", "356", "8", function(event) {
-      player.youtube = event.ref;
-      player.bindListeners();
-    });
+    swfobject.embedSWF("http://www.youtube.com/v/" + this.playlist.nextId() + "?enablejsapi=1&playerapiid=video&autoplay=1&version=3", "video", "825", "356", "8", null, null, options);
   },
 
-  bindListeners: function() {
-    // http://code.google.com/apis/youtube/js_api_reference.html#Events
-    this.youtube.addEventListener('onStateChange', function(state) {
-      console.log(state);
-      if (state == 0) { // Video ended; load the next one
-        youtube.loadById(this.playlist.nextId());
-      }
-    });
-
-    this.youtube.addEventListener('error', function(error) {
-      alert('YouTube Error ' + error);
-    });
+  // Called on YouTube video state change
+  loadNext: function(state) {
+    if (state === 0) { // Video ended; load the next one
+      this.youtube.loadVideoById(this.playlist.nextId());
+    }
   }
 });
+
+function onYouTubePlayerReady(playerId) {
+  Confluence.player.youtube = document.getElementById(playerId);
+  Confluence.player.youtube.addEventListener('onStateChange', 'Confluence.player.loadNext');
+}
